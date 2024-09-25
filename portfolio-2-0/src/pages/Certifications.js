76c1f03d2f9c205c8dc9acf933dd98711e3c5721
Kalpkumar Shah <79212981+kshah223@users.noreply.github.com>
2024-09-25 12:29:01 -0500

    Create Certifications.js

---
+++ portfolio-2-0/src/pages/Certifications.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Certifications.css';
+
+const Certifications = () => {
+  return (
+    <section id="certifications">
+      <h2>Certifications</h2>
+      <ul>
+        <li>Software Engineering Intern by HackerRank - Issued September 2024</li>
+        <li>Software Engineering by HackerRank - Issued September 2024</li>
+        <li>Frontend Developer (React) by HackerRank - Issued September 2024</li>
+        <li>React Developer Certifications - Issued July 2024</li>
+        <li>AT&T 2024 Technology Academy - Issued June 2024</li>
+        <li>Microsoft Certified: Azure Fundamentals - Issued April 2024</li>
+        <li>SQL Advanced by HackerRank - Issued December 2023</li>
+        <li>edX Verified Certificate for CS50's Introduction to Computer Science - Issued May 2023</li>
+        <li>Microsoft Certified Solutions Associate: Linux on Azure - Issued February 2023</li>
+        <li>2021 AT&T Summer Learning Academy Extern - Issued July 2021</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Certifications;
